30b4e1dbee3fc130c5fe762397571075393da2cd
Joaquin <canteroxx92@gmail.com>
2023-09-25 15:23:10 -0300

    union

--- Proyecto/resources/views/paginas/ayuda.blade.php
+++ Proyecto/resources/views/paginas/ayuda.blade.php
@@ -1,8 +1,73 @@
 @extends('menu')
 
 @section('menu')
+<div class="CuerpoPagina">
+    <header class="Cont-A1">
+        <h1>Ayuda</h1>
+        <hr>
+    </header>
+    <main class = 'Cont-A2'>
+        <section id ="Box1-a">
+            <h3>Relacionado al Uso de La pagina</h3>
+            <div class = 'Cont3'>
+                <ul >
+                    <li><details><summary>¿Duda1:Como agregar un registro en productos?</summary>
+                        <p>Texto expplicando la duda 1
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea inventore omnis aut totam, illo ipsa molestias alias laboriosam officiis est hic? Fugit voluptate officiis harum ea dignissimos sequi nam veniam!
+                        </p></details></li>
+                    <li><details><summary>¿Duda2:Donde se encuntra la opción para cambiar el tamaño de letra de mis tablas?</summary>
+                        <p>Texto expplicando la duda 2</p></details></li>
+                    <li><details><summary>¿Duda3?</summary>
+                        <p>Texto expplicando la duda 3</p></details></li>
+                    <li><details><summary>¿Duda4?</summary>
+                        <p>Texto expplicando la duda 4</p></details></li>
+                    <li><details><summary>¿Duda5?</summary>
+                        <p>Texto expplicando la duda 5</p></details></li>
+                    <li><details><summary>¿Duda6?</summary>
+                        <p>Texto expplicando la duda 6</p></details></li>
+                    <li><details><summary>¿Duda7?</summary>
+                        <p>Texto expplicando la duda 7</p></details></li>
+                    <li><details><summary>¿Duda8?</summary>
+                        <p>Texto expplicando la duda 8</p></details></li>
+                    <li><details><summary>¿Duda9?</summary>
+                        <p>Texto expplicando la duda 9</p></details></li>
+                    <li><details><summary>¿Duda10:?</summary>
+                        <p>Texto expplicando la duda 10</p></details></li>
+                </ul>
+            </div>
+        </section>
+        <section id ="Box2-a">
+            <h3>Problemas con la pagina</h3>
+            <div class = 'Cont2'>
+                <ul >
+                    <li><details><summary>¿Duda1:Tengo problema al ingresar un dato?</summary>
+                        <p>Texto expplicando la duda 1. |Reportar|</p></details></li>
+                    <li><details><summary>¿Duda2?</summary>
+                        <p>Texto expplicando la duda 2</p></details></li>
+                    <li><details><summary>¿Duda3?</summary>
+                        <p>Texto expplicando la duda 3</p></details></li>
+                    <li><details><summary>¿Duda4?</summary>
+                        <p>Texto expplicando la duda 4</p></details></li>
+                    <li><details><summary>¿Duda5?</summary>
+                        <p>Texto expplicando la duda 5</p></details></li>
+                    <li><details><summary>¿Duda6?</summary>
+                        <p>Texto expplicando la duda 6</p></details></li>
+                    <li><details><summary>¿Duda7?</summary>
+                        <p>Texto expplicando la duda 7</p></details></li>
+                    <li><details><summary>¿Duda8?</summary>
+                        <p>Texto expplicando la duda 8</p></details></li>
+                    <li><details><summary>¿Duda9?</summary>
+                        <p>Texto expplicando la duda 9</p></details></li>
+                    <li><details><summary>¿Duda10:?</summary>
+                        <p>Texto expplicando la duda 10</p></details></li>
+                </ul>
+            </div>
+        </section>
+    </main>
+    <footer class="Cont-A3">@2023-09-23 Sistema de Gestiones</footer>
+</div>
 @endsection
 
 @section('css')
-    <link rel="stylesheet" href="{{asset('css/')}}">
+    <link rel="stylesheet" href="{{asset('css/y_estilo.css')}}">
 @endsection
--- Proyecto/resources/views/paginas/home.blade.php
+++ Proyecto/resources/views/paginas/home.blade.php
@@ -1,8 +1,55 @@
 @extends('menu')
 
 @section('menu')
+<div class="CuerpoPagina">
+    <header class="C-MP1">
+        <div>
+            <img src="Logo.jpg"width='200px'>
+            <h1>Nombre de la Empresa</h1>
+        </div>
+    </header>
+    <main class="C-MP2">
+        <section id='Contedor-1'>
+            <div id ='DivCambio1'>
+                <!--PARRAFOS DESCRIPTIVO ,del mensaje del boton
+                    Usando Javascript se colocaria los correspondientes
+                    del boton que se precione , se ejecutaria el tipo de info que se cola
+                    en este apartado, mienstras estaria vacia
+
+                    La etiqueta canavas , para hacer lo del grafico-->
+                <p>lugar donde sale la el texto y grafico</p>
+                <canvas id="miGrafico" height="0rem"></canvas>
+            </div>
+        </section>
+        <section id="Contedor-2">
+            <button class="BotonMenu" id="Bot1">Estadistica</button>
+            <button class="BotonMenu" id="Bot2">Productos mas vendidos</button>
+            <button class="BotonMenu" id="Bot3">Productos menos vendidos</button>
+            <button class="BotonMenu" id="Bot4">Resumen de Registros</button>
+        </section>
+        <section id="Contedor-3">
+            <div id ='DivCambio1'> </div>
+        </section>
+        <section id="Contedor-4" >
+                <div class="C parte-1"><details><summary>Clientes</summary>
+                <p>.</p></details></div>
+                <div class="C parte-2"><details><summary>Existencia total</summary></details></div>
+                <div class="C parte-3"><details><summary>Importe Pagado</summary></details></div>
+                <div class="C parte-4"><details><summary>Provedores</summary></details></div>
+                <div class="C parte-5"><details><summary>Existencia vendida</summary></details></div>
+                <div class="C parte-6"><details><summary>Importe restante</summary></details></div>
+                <div class="C parte-7"><details><summary>Productos</summary></details></div>
+                <div class="C parte-8"><details><summary>Existencia Actual</summary></details></div>
+                <div class="C parte-9"><details><summary>Beneficio Bruto</summary></details></div>
+                <div class="C parte-10"><details><summary>Facturas</summary></details></div>
+                <div class="C parte-11"><details><summary>Importe vendido</summary></details></div>
+                <div class="C parte-12"><details><summary>Beneficio Neto</summary></details></div>
+        </section>
+    </main>
+    <footer class="C-MP3">@2023-09-21-Sistema de Gestiones</footer>
+</div>
 @endsection
 
 @section('css')
-    <link rel="stylesheet" href="{{asset('css/')}}">
+    <link rel="stylesheet" href="{{asset('css/y_estilo.css')}}">
 @endsection
