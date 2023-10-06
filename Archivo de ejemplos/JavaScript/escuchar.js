15aac206ae4bcb5f0848b1b77378e0d6a0c47c4e
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2023-10-06 13:58:47 -0300

    Eliminar & Limpiar %archivos fijos
    ¿Qué se hizo?
    - Se eliminaron los archivos antiguos. Ya que había archivos incompletos o estaban confusos de compresión.
    - Reorganice los archivos, y actualice .los archivos en que estoy trabajando.
    - Se dejaron los archivos de css(los de estilos y los HTML, archivos que había trabajado  entorno a la pagina.)

--- Archivo de ejemplos/JavaScript/escuchar.js
+++
@@ -1,5 +0,0 @@
-
-/*ESCUCHAR EVENTO DE LA PAGINA "paginaGENERAL" */
-const imgUser = document.querySelector('#ImagenUsuario')
-imgUser.addEventListener('mouseover', function() {Cambiar_Img(this,'on')} )
-imgUser.addEventListener('mouseout', function() {Cambiar_Img(this,'off')} )
