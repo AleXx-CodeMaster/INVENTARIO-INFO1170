9ba1b0f35af6410b3b6725b45453a837785c9263
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2023-10-23 22:18:27 -0300

    error &
    Producto ah un error , se me copiaron todos los demás archivos del proyecto en mi rama , migrare a la beta-rama-yaninna , para continuar trabajando .

---
+++ Proyecto/storage/framework/views/2bced72efee8c1dc50399824e86c541b.php
@@ -0,0 +1,98 @@
+<?php $__env->startSection('menu'); ?>
+    <header class="tittle">
+        <h1>Proveedores</h1>
+        <hr>
+    </header>
+    <div>
+    </div> <br>
+    <div class="add">
+        <input type="submit" value="Cliente Nuevo">
+    </div> <br> <br> <br>
+    <nav class="busqueda">
+        <input type="submit" name="" id="" value="Buscar por nombre">
+        <input type="submit" name="" id="" value="Buscar por correo">
+        <input type="submit" name="" id="" value="Buscar por telefono">
+    </nav>
+    <section class="proveedores">
+        <header class="prov-dat">
+        </header>
+        <div class="prov2">
+            <table class="table-prov2">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Correo Electronico</th>
+                    <th>Telefono</th>
+                    <th>Direccion</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </table>
+        </div>
+        <div class="prov">
+                <table class="table-prov">
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56963028619</th>
+                        <th>Balmaceda194</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                    <tr>
+                        <th>Joaquin</th>
+                        <th>canteroxx92@gmail.com</th>
+                        <th>+56928188128</th>
+                        <th>Pastor Oscar Alarcon 181</th>
+                        <th class="li1"><li></li></th>
+                        <th class="li2"><li></li></th>
+                    </tr>
+                </table>
+        </div>
+    </section>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('css'); ?>
+    <link rel="stylesheet" href="<?php echo e(asset('css/proveedores.css')); ?>">
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\cante\OneDrive\Escritorio\Proyecto\INVENTARIO-INFO1170\Proyecto\resources\views/paginas/proveedores.blade.php ENDPATH**/ ?>
