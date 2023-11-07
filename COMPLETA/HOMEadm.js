f04bb1cfeb6f80345a12044ed6f5c2794795db01
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2023-11-07 02:46:46 -0300

    Add files via upload

---
+++ COMPLETA/HOMEadm.js
@@ -0,0 +1,24 @@
+const AreaAdmnistrador = document.querySelector('.Area__Seleccion')
+
+AreaAdmnistrador.innerHTML = `
+<h3>Área de Trabajo</h3>
+<div class="AREA_S">
+<ul>
+    <li><a href="#">Registro de Productos</a></li>
+    <li><a href="#">Añadir Productos</a></li>
+    <li><a href="#">Eliminar Productos</a></li></ul>
+<ul>
+    <li><a href="#">Registro de Proveedores</a></li>
+    <li><a href="#">Añadir Proveedores</a></li>
+    <li><a href="#">Proveedores posteriores</a></li></ul>
+<ul id="Area__T">
+    <li><a href="#">Registro Trabajadores</a></li>
+    <li><a href="#">Añadir nuevo Trabajador</a></li>
+    <li><a href="#">Trabajadores Posteriores</a></li>
+</ul>
+<ul>
+    <li><a href="#">Categoria</a></li>
+    <li><a href="#">Almacenamiento</a></li>
+    <li><a href="#">Panel de Mensajes</a></li></ul>
+</div>
+`
---
+++ COMPLETA/HOMEempl.js
@@ -0,0 +1,19 @@
+const AreaEmpleado = document.querySelector('.Area__Seleccion')
+
+AreaEmpleado.innerHTML = `
+<h3>Área de Trabajo</h3>
+<div class="AREA_S">
+<ul>
+    <li><a href="#">Registro de Productos</a></li>
+    <li><a href="#">Añadir Productos</a></li>
+    <li><a href="#">Eliminar Productos</a></li></ul>
+<ul>
+    <li><a href="#">Registro de Proveedores</a></li>
+    <li><a href="#">Añadir Proveedores</a></li>
+    <li><a href="#">Proveedores posteriores</a></li></ul>
+<ul>
+    <li><a href="#">Categoria</a></li>
+    <li><a href="#">Almacenamiento</a></li>
+    <li><a href="#">Panel de Mensajes</a></li></ul>
+</div>
+`
