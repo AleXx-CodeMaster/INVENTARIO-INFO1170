e3c4babbee47fe111615b86486cd82c100bc46eb
ALEXIS MONSALVE <125103323+AleXx-CodeMaster@users.noreply.github.com>
2023-11-22 12:58:18 -0300

    Delete NexusFinal directory

--- NexusFinal/public/js/antiguos.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("BotonAntiguo").addEventListener("click", function() {
-    var modal = document.getElementById("antiguo");
-    modal.style.display = "block";
-});
-document.getElementById("antiguo").addEventListener("click", function(e) {
-    if (e.target === this || e.target.id === "CerrarModal") {
-        this.style.display = "none";
-    }
-});
--- NexusFinal/public/js/ayuda.js
+++
@@ -1,8 +0,0 @@
-function toggleHelp(helpId) {
-    var helpContent = document.getElementById(helpId);
-    if (helpContent.style.display === "none" || helpContent.style.display === "") {
-        helpContent.style.display = "block";
-    } else {
-        helpContent.style.display = "none";
-    }
-}
